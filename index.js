1b730483faa6012713759ab1453cc40a6aa27946
Jon Quach <hello@jonquach.com>
2018-10-17 17:50:53 -0400

    @seedcss imports (#4)
    * SeedCSS: Fix import paths
    
    This update fixes the `@import` path for `@seedcss` based packs.
    
    Before:
    ```
    @import "pack/@seedcss/seed-reset/_index";
    ```
    
    After:
    ```
    @import "pack/seed-reset/_index";
    ```
    
    * Update dependencies
    
    * Bump Travis Node version from 4 -> 8

--- index.js
+++ index.js
@@ -1,86 +1,84 @@
 // Seed Packer
-'use strict';
+"use strict";
 
 // Dependencies
-var findRoot = require('find-root');
-var fs = require('fs');
-var glob = require('glob');
-var packfinder = require('seed-packfinder');
-var path = require('path');
-var root = findRoot(process.cwd());
+const findRoot = require("find-root");
+const fs = require("fs");
+const glob = require("glob");
+const packfinder = require("seed-packfinder");
+const path = require("path");
+const root = findRoot(process.cwd());
 
+const SEEDCSS_NAMESPACE = "@seedcss";
+const SEEDCSS_PATH = `${SEEDCSS_NAMESPACE}/`;
 
 // Methods
 
-var getPathFile = function(file) {
+const getPathFile = function(file) {
   if (!file) {
     return false;
   }
 
-  if (file.indexOf('_seed-packs.scss') < 0) {
-    console.log('A non-glob path must include _seed-pack.scss');
+  if (file.indexOf("_seed-packs.scss") < 0) {
+    console.log("A non-glob path must include _seed-pack.scss");
     process.exit(1);
     return false;
   }
 
   file = path.join(root, file);
-  var stat = fs.statSync(file).isFile();
+  const stat = fs.statSync(file).isFile();
 
   if (!stat) {
-    console.log('seed-packer could not find ' + file + '.');
-    console.log('Please double check to make sure the path to _seed-packs.scss is correct.');
-    console.log('If you\'re not sure, you can try using seed-packer with an argument.');
+    console.log("seed-packer could not find " + file + ".");
+    console.log(
+      "Please double check to make sure the path to _seed-packs.scss is correct."
+    );
+    console.log(
+      "If you're not sure, you can try using seed-packer with an argument."
+    );
     return process.exit(1);
-  }
-  else {
+  } else {
     return file;
   }
 };
 
-
-var getPackFile = function(filePath) {
+const getPackFile = function(filePath) {
   // Default glob path
-  filePath = filePath ? filePath : '**/*.scss';
+  filePath = filePath ? filePath : "**/*.scss";
 
-  var file = false;
-  var options = {
-    ignore: [
-      'bower_components/**/*',
-      'node_modules/**/*'
-    ]
+  let file = false;
+  const options = {
+    ignore: ["bower_components/**/*", "node_modules/**/*"]
   };
 
-  if (filePath.indexOf('*.scss') >= 0) {
-    var scssFiles = glob.sync(filePath, options);
+  if (filePath.indexOf("*.scss") >= 0) {
+    const scssFiles = glob.sync(filePath, options);
 
     if (!scssFiles.length) {
       return false;
     }
 
-    for(var i = 0, len = scssFiles.length; i < len; i++) {
-      var f = scssFiles[i];
-      if (f.indexOf('_seed-packs.scss') >= 0) {
+    for (let i = 0, len = scssFiles.length; i < len; i++) {
+      const f = scssFiles[i];
+      if (f.indexOf("_seed-packs.scss") >= 0) {
         file = f;
         break;
       }
     }
-  }
-  else {
+  } else {
     file = getPathFile(filePath);
   }
 
   return file;
 };
 
-
-var getResetPacks = function(packs) {
+const getResetPacks = function(packs) {
   return packs.filter(function(pack) {
-    return pack.indexOf('reset') >= 0;
+    return pack.indexOf("reset") >= 0;
   });
 };
 
-
-var prioritizeResetPacks = function(packs) {
+const prioritizeResetPacks = function(packs) {
   var resetPacks = getResetPacks(packs);
   if (resetPacks.length) {
     resetPacks.forEach(function(pack) {
@@ -91,8 +89,7 @@ var prioritizeResetPacks = function(packs) {
   return packs;
 };
 
-
-var addPacks = function(packs, file) {
+const addPacks = function(packs, file) {
   if (!packs || !file) {
     return false;
   }
@@ -100,11 +97,13 @@ var addPacks = function(packs, file) {
   // Prioritize reset packs
   packs = prioritizeResetPacks(packs);
 
-  var template = '// Seed packs\n';
-  template += '// Automagically added by seed-packer <3\n\n';
+  let template = "// Seed packs\n";
+  template += "// Automagically added by seed-packer <3\n\n";
 
-  for(var i = 0, len = packs.length; i < len; i++) {
-    var p = packs[i];
+  for (let i = 0, len = packs.length; i < len; i++) {
+    let p = packs[i];
+    p = p.replace(SEEDCSS_PATH, "");
+
     template += '@import "pack/' + p + '/_index";\n';
   }
   // Write to file
@@ -113,14 +112,13 @@ var addPacks = function(packs, file) {
   return template;
 };
 
-
-var seedPacker = function(filePath) {
-  var packs = packfinder.find();
+const seedPacker = function(filePath) {
+  const packs = packfinder.find();
   if (!packs) {
     return false;
   }
 
-  var file = getPackFile(filePath);
+  const file = getPackFile(filePath);
 
   if (!file) {
     return;
--- test/packer.js
+++ test/packer.js
@@ -1,21 +1,21 @@
 // Test :: Packer
-var assert = require('chai').assert;
-var findRoot = require('find-root');
-var fs = require('fs');
-var packer = require('../index');
-var path = require('path');
-var root = findRoot(process.cwd());
+const assert = require('chai').assert;
+const findRoot = require('find-root');
+const fs = require('fs');
+const packer = require('../index');
+const path = require('path');
+const root = findRoot(process.cwd());
 
 beforeEach(function(done) {
   // Wipes _seed-pack.scss to blank
-  var file = '/test/scss/_seed-packs.scss';
+  let file = '/test/scss/_seed-packs.scss';
   file = path.join(root, file);
   fs.writeFileSync(file, '');
   done();
 });
 
 describe('packer: writes', function() {
-  var output = packer();
+  const output = packer();
 
   it('should automatically add seed packs (from package.json)', function(done) {
     assert.equal(true, output.includes('seed-breakpoints'));
@@ -23,7 +23,7 @@ describe('packer: writes', function() {
   });
 
   it('should automatically add @seedcss/ seed packs (from package.json)', function(done) {
-    assert.equal(true, output.includes('pack/@seedcss/seed-color-scheme/_index'));
+    assert.equal(true, output.includes('pack/seed-color-scheme/_index'));
     done();
   });
 
